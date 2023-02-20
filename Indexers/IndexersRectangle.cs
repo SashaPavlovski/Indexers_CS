c68ac2c93b08f6e8c02d54622a908aa9a8477c83
SashaPavlovski <111590243+SashaPavlovski@users.noreply.github.com>
2023-02-20 13:15:12 +0200

    Indexers

---
+++ Indexers/IndexersRectangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Indexers
+{
+    public class IndexersRectangle
+    {
+        private Rectangle Rectangle { get; set; }
+
+        private Dictionary<string, Point> PointList;
+        public IndexersRectangle(Rectangle rectangle)
+        {
+            Rectangle = rectangle;
+
+            PointList = new Dictionary<string, Point>();
+        }
+
+        public Point this[string Key]
+        {
+            get
+            {
+                return PointList[Key];
+            }
+            set
+            {
+                if (!PointList.ContainsKey(Key))
+                {
+                    if (Rectangle.Contains(value))
+                    {
+                        PointList.Add(Key, value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("The point exist");
+                    }
+                }
+            }
+        }
+
+        public void AddPoint(string name, int x, int y)
+        {
+            Point p = new Point(x,y);
+
+            Rectangle NewRectangle = new Rectangle(p, ((Size)p));
+
+            if (!PointList.ContainsKey(name))
+            {
+                if (Rectangle.Contains(NewRectangle))
+                {
+                    PointList.Add(name, p);
+                }
+                else
+                {
+                    Console.WriteLine("the point exist");
+                }
+            }
+        }
+    }
+}
---
+++ Indexers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace Indexers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Point point = new Point(10, 10);
+
+            Rectangle rectangle = new Rectangle(10,10,10,10);
+
+            IndexersRectangle indexersRectangle = new IndexersRectangle(rectangle);
+
+            indexersRectangle.AddPoint("a", 2, 3);
+
+            indexersRectangle.AddPoint("a", 7, 3);
+
+            indexersRectangle.AddPoint("b", 15, 15);
+
+            indexersRectangle["c"] = new Point(6, 6);
+
+            indexersRectangle["a"] = new Point(2, 6);
+
+        }
+    }
+}
